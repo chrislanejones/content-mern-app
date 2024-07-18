bf0b0065baf0e5ad3c4f96913e07544282f7daeb
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-18 03:13:27 -0400

    Express - User Controller Functions

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,11 +1,46 @@
-import expressAsyncHandler from "express-async-handler";
+import AsyncHandler from "express-async-handler";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
 // @access  Public
 
-const authUser = expressAsyncHandler(async (req, res) => {
+const authUser = AsyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc    Register a new user
+// @route   POST /api/users/auth
+// @access  Public
+
+const registerUser = AsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc    Logout new user
+// @route   POST /api/users/auth
+// @access  Public
+const logoutUser = AsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = AsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get User Profile" });
+});
+
+// @desc    Update user profile
+// @route   Put /api/users/profile
+// @access  Public
+const updateUserProfile = AsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,16 @@
 import express from "express";
-import { authUser } from "../controllers/userControllers.js";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userControllers.js";
 const router = express.Router();
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
