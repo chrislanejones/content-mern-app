a91d68adb6012bda3a28261d3cf22e5743a5cc94
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-23 22:52:01 -0400

    Added BootStrap with Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand>MERN APP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/login">
+                <FaSignInAlt /> Sign In
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaSignOutAlt /> Sign Up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
